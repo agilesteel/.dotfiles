bab59064ee2452d25f7b523c715baadb88ed40f8
Vladyslav Pekker <agilesteel@gmail.com>
2022-01-13 13:15:39 -0300

    housekeeping

--- setup/seed-install.sh
+++ setup/seed-install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# install nix
-# curl -L https://nixos.org/nix/install | sh
-
-# source ~/.bashrc
-
 while read package; do
   nix-env -iA nixpkgs.$package
 done < ~/.dotfiles/setup/nixpkgs
