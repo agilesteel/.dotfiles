4789370749a2e93f699df7833eadd74af8723a72
Vlad Pekker <vpekker@avantstay.com>
2022-05-18 12:40:56 -0300

    housekeeping

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -20,14 +20,14 @@ require("settings.galaxyline").setup()
 -- require("nvim-autopairs").setup()
 
 require("nvim-treesitter.configs").setup({
-  playground = { enable = true },
-  query_linter = {
-    enable = true,
-    use_virtual_text = true,
-    lint_events = { "BufWrite", "CursorHold" },
-  },
-  ensure_installed = "maintained",
-  highlight = { enable = true },
+  -- playground = { enable = true },
+  -- query_linter = {
+    -- enable = true,
+    -- use_virtual_text = true,
+    -- lint_events = { "BufWrite", "CursorHold" },
+  -- },
+  -- ensure_installed = "maintained",
+  -- highlight = { enable = true },
 })
 
 require("lspsaga").init_lsp_saga({
