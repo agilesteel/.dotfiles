e179a4811f81864ffa0bc811dabccde5df64684e
Vladyslav Pekker <agilesteel@gmail.com>
2022-10-18 18:05:03 -0300

    housekeeping

--- stow/p10k/.p10k.zsh
+++ stow/p10k/.p10k.zsh
@@ -97,7 +97,7 @@
     # vpn_ip                # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
-    # ram                   # free RAM
+    ram                   # free RAM
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
