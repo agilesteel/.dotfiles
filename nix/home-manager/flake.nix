fa933e1b7e00f85c3a787551fba9b381808c4a77
Vladyslav Pekker <agilesteel@gmail.com>
2023-03-01 23:46:24 -0300

    housekeeping

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -1,21 +1,19 @@
 {
-  description = "Home Manager configuration of Vlad";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    hm = {
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, hm, ... }:
+  outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations.vlad = hm.lib.homeManagerConfiguration {
+      homeConfigurations.vlad = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         modules = [
