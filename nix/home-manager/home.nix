262c60d5de9a8528d67f0a9b14829702736d2ec6
Vladyslav Pekker <agilesteel@gmail.com>
2023-03-04 15:41:16 -0300

    housekeeping

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -1,63 +1,66 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "vlad";
-  home.homeDirectory = "/home/vlad";
+  home = {
+    homeDirectory = "/home/vlad";
 
-  home.stateVersion = "22.11";
+    packages = with pkgs; [
+      asciiquarium
+      bat
+      bottom
+      cachix
+      cmatrix
+      coursier
+      cowsay
+      delta
+      direnv
+      docker
+      docker-compose
+      doppler
+      duf
+      fd
+      figlet
+      fx
+      fzf
+      gh
+      git
+      glow
+      graalvm17-ce
+      graphviz
+      gum
+      heroku
+      htop
+      ipfetch
+      keychain
+      less
+      lolcat
+      mosh
+      ncdu
+      neofetch
+      neovim
+      nix-direnv
+      ookla-speedtest
+      pulumi
+      ripgrep
+      ripgrep-all
+      scala-cli
+      sl
+      stow
+      sublime4
+      tig
+      tree
+      wl-clipboard
+      zsh
+    ];
 
-  # Let Home Manager install and manage itself.
+    stateVersion = "22.11";
+    username = "vlad";
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = _: true;
+  };
+
   programs.home-manager.enable = true;
-
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnfreePredicate = _: true;
-
-  home.packages = with pkgs; [
-    asciiquarium
-    bat
-    bottom
-    cachix
-    cmatrix
-    coursier
-    cowsay
-    delta
-    direnv
-    docker
-    docker-compose
-    doppler
-    duf
-    fd
-    figlet
-    fx
-    fzf
-    gh
-    git
-    glow
-    graalvm17-ce
-    graphviz
-    gum
-    heroku
-    htop
-    ipfetch
-    keychain
-    less
-    lolcat
-    mosh
-    ncdu
-    neofetch
-    neovim
-    nix-direnv
-    ookla-speedtest
-    pulumi
-    ripgrep
-    ripgrep-all
-    scala-cli
-    sl
-    stow
-    sublime4
-    tig
-    tree
-    wl-clipboard
-    zsh
-  ];
 }
