cd698699b3bcab3beccbe5f606c2b74a3adcd193
Vladyslav Pekker <agilesteel@gmail.com>
2023-05-07 10:12:16 -0300

    feat: add multi-system support

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -2,6 +2,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,20 +15,30 @@
 
   outputs = {
     nixpkgs,
+    flake-utils,
     home-manager,
     ...
   }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    formatter.${system} = pkgs.alejandra;
+    supportedSystems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-linux"
+      "x86_64-darwin"
+    ];
+  in
+    flake-utils.lib.eachSystem supportedSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.alejandra;
 
-    homeConfigurations.vlad = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
+        packages.homeConfigurations.vlad = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-      modules = [
-        ./home.nix
-      ];
-    };
-  };
+          modules = [
+            ./home.nix
+          ];
+        };
+      }
+    );
 }
