936e6feb8cada89e447cfa95dc4f725ed870d483
Vladyslav Pekker <agilesteel@gmail.com>
2023-12-16 00:47:24 -0300

    housekeeping

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -138,7 +138,7 @@ cmd([[autocmd TermOpen * startinsert]])
 cmd([[augroup lsp]])
 cmd([[autocmd!]])
 cmd([[autocmd FileType scala setlocal omnifunc=v:lua.vim.lsp.omnifunc]])
-cmd([[autocmd FileType scala,sbt lua require("metals").initialize_or_attach(Metals_config)]])
+-- cmd([[autocmd FileType scala,sbt lua require("metals").initialize_or_attach(Metals_config)]])
 cmd([[augroup end]])
 
 -- Needed to esnure float background doesn't get odd highlighting
