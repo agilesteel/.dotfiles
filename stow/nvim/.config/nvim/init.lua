730a2aad919e1e2c86ecc682af94fc6ec99fc0df
Vladyslav Pekker <agilesteel@gmail.com>
2024-08-10 18:29:31 -0300

    housekeeping

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -527,39 +527,39 @@ require('lazy').setup({
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
           -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          -- map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          -- map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          -- map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          -- map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          -- map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          -- map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
@@ -1008,6 +1008,186 @@ require('lazy').setup({
     end,
     opts = {},
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-vsnip' },
+      { 'hrsh7th/vim-vsnip' },
+    },
+    opts = function()
+      local cmp = require 'cmp'
+      local conf = {
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
+        },
+        snippet = {
+          expand = function(args)
+            -- Comes from vsnip
+            vim.fn['vsnip#anonymous'](args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
+          -- None of this made sense to me when first looking into this since there
+          -- is no vim docs, but you can't have select = true here _unless_ you are
+          -- also using the snippet stuff. So keep in mind that if you remove
+          -- snippets you need to remove this select
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+        },
+      }
+      return conf
+    end,
+  },
+  {
+    'scalameta/nvim-metals',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'j-hui/fidget.nvim',
+        opts = {},
+      },
+      {
+        'mfussenegger/nvim-dap',
+        config = function(self, opts)
+          -- Debug settings if you're using nvim-dap
+          local dap = require 'dap'
+
+          dap.configurations.scala = {
+            {
+              type = 'scala',
+              request = 'launch',
+              name = 'RunOrTest',
+              metals = {
+                runType = 'runOrTestFile',
+                --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+              },
+            },
+            {
+              type = 'scala',
+              request = 'launch',
+              name = 'Test Target',
+              metals = {
+                runType = 'testTarget',
+              },
+            },
+          }
+        end,
+      },
+    },
+    ft = { 'scala', 'sbt', 'java' },
+    opts = function()
+      local metals_config = require('metals').bare_config()
+
+      -- Example of settings
+      metals_config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { 'akka.actor.typed.javadsl', 'com.github.swagger.akka.javadsl' },
+      }
+
+      -- *READ THIS*
+      -- I *highly* recommend setting statusBarProvider to either "off" or "on"
+      --
+      -- "off" will enable LSP progress notifications by Metals and you'll need
+      -- to ensure you have a plugin like fidget.nvim installed to handle them.
+      --
+      -- "on" will enable the custom Metals status extension and you *have* to have
+      -- a have settings to capture this in your statusline or else you'll not see
+      -- any messages from metals. There is more info in the help docs about this
+      metals_config.init_options.statusBarProvider = 'off'
+
+      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+      metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      metals_config.on_attach = function(client, bufnr)
+        require('metals').setup_dap()
+
+        -- LSP mappings
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+        vim.keymap.set('n', 'gds', vim.lsp.buf.document_symbol)
+        vim.keymap.set('n', 'gws', vim.lsp.buf.workspace_symbol)
+        vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run)
+        vim.keymap.set('n', '<leader>sh', vim.lsp.buf.signature_help)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+        vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+
+        vim.keymap.set('n', '<leader>ws', function()
+          require('metals').hover_worksheet()
+        end)
+
+        -- all workspace diagnostics
+        vim.keymap.set('n', '<leader>aa', vim.diagnostic.setqflist)
+
+        -- all workspace errors
+        vim.keymap.set('n', '<leader>ae', function()
+          vim.diagnostic.setqflist { severity = vim.diagnostic.severity.E }
+        end)
+
+        -- all workspace warnings
+        vim.keymap.set('n', '<leader>aw', function()
+          vim.diagnostic.setqflist { severity = vim.diagnostic.severity.W }
+        end)
+
+        -- buffer diagnostics only
+        vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist)
+
+        vim.keymap.set('n', '[c', function()
+          vim.diagnostic.goto_prev { wrap = false }
+        end)
+
+        vim.keymap.set('n', ']c', function()
+          vim.diagnostic.goto_next { wrap = false }
+        end)
+
+        -- Example mappings for usage with nvim-dap. If you don't use that, you can
+        -- skip these
+        vim.keymap.set('n', '<leader>dc', function()
+          require('dap').continue()
+        end)
+
+        vim.keymap.set('n', '<leader>dr', function()
+          require('dap').repl.toggle()
+        end)
+
+        vim.keymap.set('n', '<leader>dK', function()
+          require('dap.ui.widgets').hover()
+        end)
+
+        vim.keymap.set('n', '<leader>dt', function()
+          require('dap').toggle_breakpoint()
+        end)
+
+        vim.keymap.set('n', '<leader>dso', function()
+          require('dap').step_over()
+        end)
+
+        vim.keymap.set('n', '<leader>dsi', function()
+          require('dap').step_into()
+        end)
+
+        vim.keymap.set('n', '<leader>dl', function()
+          require('dap').run_last()
+        end)
+      end
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = self.ft,
+        callback = function()
+          require('metals').initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
   },
 }, {
   ui = {
