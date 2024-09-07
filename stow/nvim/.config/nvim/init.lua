8a727fd36c42b04895f9c41a15f78759bef3b303
Vladyslav Pekker <agilesteel@gmail.com>
2024-09-06 22:37:54 -0300

    housekeeping

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -1313,6 +1313,11 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
