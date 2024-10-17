7c9afdc4e91e7b3d79d5de20308ad0ed01436a0c
Vladyslav Pekker <agilesteel@gmail.com>
2024-10-17 20:37:59 -0300

    housekeeping

--- stow/p10k/.p10k.zsh
+++ stow/p10k/.p10k.zsh
@@ -103,7 +103,7 @@
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    # time                  # current time
+    time                  # current time
     # =========================[ Line #2 ]=========================
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
