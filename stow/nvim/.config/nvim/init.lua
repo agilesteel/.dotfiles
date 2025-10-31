75c00bba342645fc9ce3b21b206525d3c63a48c7
Vladyslav Pekker <agilesteel@gmail.com>
2025-10-31 09:56:22 -0300

    housekeeping

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -328,7 +328,6 @@ require('lazy').setup({
   -- Optional kickstart plugins
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.gitsigns',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.neo-tree',
